8bd1b667d87ea52d11d17cfc4bf9653306b1b910
Ghanashyam behera <ghanashyambehera007@gmail.com>
2026-01-21 15:59:38 +0530

    - updated app strings ..

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/di/injection.dart';
+import 'core/utils/app_strings.dart';
 import 'features/todo/presentation/cubit/todo_cubit.dart';
 import 'features/todo/presentation/pages/todo_page.dart';
 
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Todo App',
+        title: AppStrings.toDoApp,
         theme: ThemeData(
           colorScheme: .fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
